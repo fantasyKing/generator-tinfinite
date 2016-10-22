54f359af60896bcfb405fc9fc46d751b45da38b6
zhangGuojin <zhanggjjim@163.com>
2016-10-22 15:56:22 +0800

    first commit

---
+++ generators/index.js
@@ -0,0 +1,8 @@
+const path       = require('path');
+const yosay      = require('yosay');
+const to         = require('to-case');
+const generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+  
+});
